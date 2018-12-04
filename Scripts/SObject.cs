634d6b3da55d9d043f425b24d757011853fd8ff5
Anthony Romrell <anthonyromrell@mac.com>
2018-12-04 00:38:30 -0700

    Folders and Example

---
+++ Scripts/SObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "DangFun/Asset")]
+public class SObject : ScriptableObject
+{
+   public UnityEvent Event;
+
+   private void OnEnable()
+   {
+      Event.Invoke();
+   }
+}
